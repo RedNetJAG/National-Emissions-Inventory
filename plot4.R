b9c86d96ee99a0e9c77256fb636bf5084faa66f4
jagomez <RedNet@openmailbox.org>
2014-10-23 13:54:00 +0200

    Subida plot4

--- plot4.R
+++ plot4.R
@@ -12,19 +12,29 @@
 ##
 ##################################################################################
 
+library(ggplot2)
+
 ## Read data from files
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## Total PM2.5 emissions for each of the years
-TotalPM25 <- tapply(NEI$Emissions, NEI$year, sum)
+## subset data for Coal Combustion
+Coal <- subset(SCC, EI.Sector %in% c("Fuel Comb - Comm/Instutional - Coal", 
+                "Fuel Comb - Electric Generation - Coal", 
+                "Fuel Comb - Industrial Boilers, ICEs - Coal"))
+rm(SCC)
+CoalNEI <- subset(NEI, NEI$SCC %in%  Coal$SCC)
+rm(NEI)
+
+## Total Coal Combustion emissions for each of the years
+TotalCoalComb <- aggregate(CoalNEI["Emissions"], list(year = CoalNEI$year), sum)
 
 ## Open Graphic Device PNG
-png(filename = "plot4.png")
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
 
 ##Generates the plot
-plot(names(TotalPM25), TotalPM25, type = "l", 
-     main = "Total PM2.5 Emissions by Year in the US", xlab = "Year", ylab = "Emissions")
+plot(TotalCoalComb, type="l", main = "Emissions from Coal Combustion for the US",
+     xlim = c(1999, 2008))
 
 ##Close Graphic Device
 dev.off()
