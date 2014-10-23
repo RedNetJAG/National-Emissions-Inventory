73ed6d8840ed50d47a0b48ce60e1ec0600902b2c
jagomez <RedNet@openmailbox.org>
2014-10-23 12:59:04 +0200

    Subida plot3

--- plot4.R
+++ plot4.R
@@ -2,35 +2,29 @@
 ## Exploratory Data Analysis
 ## by Jeff Leek, PhD, Brian Caffo, PhD, Roger D. Peng, PhD
 ## 
-## plot3.R by JAGomez
+## plot4.R by JAGomez
 ## 23-Oct-2014
 ##
-## plot3.R that does the following:
+## plot4.R that does the following:
 ## 1. Read Data and subset it
 ## 2. Open Graphic Device PNG
 ## 3. Create plot
 ##
 ##################################################################################
 
-library(ggplot2)
-
 ## Read data from files
 NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-## subset data
-Baltimore <- subset (NEI, fips == "24510")
-rm(NEI)
-
-## Total PM2.5 emissions for every type of source for each of the years
-TotalPM25 <- aggregate(Baltimore["Emissions"], list(type = Baltimore$type, 
-                        year = Baltimore$year), sum)
+## Total PM2.5 emissions for each of the years
+TotalPM25 <- tapply(NEI$Emissions, NEI$year, sum)
 
 ## Open Graphic Device PNG
-png(filename = "plot3.png")
+png(filename = "plot4.png")
 
 ##Generates the plot
-qplot(year, Emissions, data=TotalPM25, color=type, geom ="line") + 
-        ggtitle("Total Emissions by Source Type in Baltimore")
+plot(names(TotalPM25), TotalPM25, type = "l", 
+     main = "Total PM2.5 Emissions by Year in the US", xlab = "Year", ylab = "Emissions")
 
 ##Close Graphic Device
 dev.off()
